7bc6ab32ecd69eca3ea97b904295acf65ace9449
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2026-06-22 21:45:28 +0200

    Add avm_decrease

---
+++ sim/tb_avm_decrease/tb_avm_decrease.vhd
@@ -0,0 +1,153 @@
+-- ---------------------------------------------------------------------------------------
+-- Description: Verify avm_sim
+-- ---------------------------------------------------------------------------------------
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity tb_avm_decrease is
+  generic (
+    G_BURST_WIDTH      : positive;
+    G_MAX_BURST        : positive;
+    G_DEBUG            : boolean;
+    G_PAUSE_SIZE       : integer;
+    G_TIMEOUT_MAX      : natural;
+    G_SLAVE_ADDR_SIZE  : positive;
+    G_SLAVE_DATA_SIZE  : positive;
+    G_MASTER_ADDR_SIZE : positive;
+    G_MASTER_DATA_SIZE : positive
+  );
+end entity tb_avm_decrease;
+
+architecture simulation of tb_avm_decrease is
+
+  signal clk : std_logic := '1';
+  signal rst : std_logic := '1';
+
+  signal s_write         : std_logic;
+  signal s_read          : std_logic;
+  signal s_address       : std_logic_vector(G_SLAVE_ADDR_SIZE - 1 downto 0);
+  signal s_writedata     : std_logic_vector(G_SLAVE_DATA_SIZE - 1 downto 0);
+  signal s_byteenable    : std_logic_vector(G_SLAVE_DATA_SIZE / 8 - 1 downto 0);
+  signal s_burstcount    : std_logic_vector(G_BURST_WIDTH - 1 downto 0);
+  signal s_readdata      : std_logic_vector(G_SLAVE_DATA_SIZE - 1 downto 0);
+  signal s_readdatavalid : std_logic;
+  signal s_waitrequest   : std_logic;
+
+  signal m_write         : std_logic;
+  signal m_read          : std_logic;
+  signal m_address       : std_logic_vector(G_MASTER_ADDR_SIZE - 1 downto 0);
+  signal m_writedata     : std_logic_vector(G_MASTER_DATA_SIZE - 1 downto 0);
+  signal m_byteenable    : std_logic_vector(G_MASTER_DATA_SIZE / 8 - 1 downto 0);
+  signal m_burstcount    : std_logic_vector(G_BURST_WIDTH - 1 downto 0);
+  signal m_readdata      : std_logic_vector(G_MASTER_DATA_SIZE - 1 downto 0);
+  signal m_readdatavalid : std_logic;
+  signal m_waitrequest   : std_logic;
+
+begin
+
+  --------------------------------
+  -- Clock and Reset
+  --------------------------------
+
+  clk <= not clk after 5 ns;
+  rst <= '1', '0' after 100 ns;
+
+
+  --------------------------------
+  -- Instantiate DUT
+  --------------------------------
+
+  avm_decrease_inst : entity work.avm_decrease
+    generic map (
+      G_BURST_WIDTH         => G_BURST_WIDTH,
+      G_SLAVE_ADDRESS_SIZE  => G_SLAVE_ADDR_SIZE,
+      G_SLAVE_DATA_SIZE     => G_SLAVE_DATA_SIZE,
+      G_MASTER_ADDRESS_SIZE => G_MASTER_ADDR_SIZE,
+      G_MASTER_DATA_SIZE    => G_MASTER_DATA_SIZE
+    )
+    port map (
+      clk_i             => clk,
+      rst_i             => rst,
+      s_write_i         => s_write,
+      s_read_i          => s_read,
+      s_address_i       => s_address,
+      s_writedata_i     => s_writedata,
+      s_byteenable_i    => s_byteenable,
+      s_burstcount_i    => s_burstcount,
+      s_readdata_o      => s_readdata,
+      s_readdatavalid_o => s_readdatavalid,
+      s_waitrequest_o   => s_waitrequest,
+      m_write_o         => m_write,
+      m_read_o          => m_read,
+      m_address_o       => m_address,
+      m_writedata_o     => m_writedata,
+      m_byteenable_o    => m_byteenable,
+      m_burstcount_o    => m_burstcount,
+      m_readdata_i      => m_readdata,
+      m_readdatavalid_i => m_readdatavalid,
+      m_waitrequest_i   => m_waitrequest
+    );
+
+
+  --------------------------------
+  -- Generate stimuli
+  --------------------------------
+
+  avm_master_sim_inst : entity work.avm_master_sim
+    generic map (
+      G_BURST_WIDTH       => G_BURST_WIDTH,
+      G_MAX_BURST         => G_MAX_BURST,
+      G_RANDOM_BYTEENABLE => false,
+      G_SEED              => X"DEADBEEFC007BABE",
+      G_NAME              => "MASTER",
+      G_DEBUG             => G_DEBUG,
+      G_OFFSET            => 1234,
+      G_TIMEOUT_MAX       => 0,
+      G_ADDR_SIZE         => G_SLAVE_ADDR_SIZE,
+      G_DATA_SIZE         => G_SLAVE_DATA_SIZE
+    )
+    port map (
+      clk_i             => clk,
+      rst_i             => rst,
+      m_waitrequest_i   => s_waitrequest,
+      m_write_o         => s_write,
+      m_read_o          => s_read,
+      m_address_o       => s_address,
+      m_writedata_o     => s_writedata,
+      m_byteenable_o    => s_byteenable,
+      m_burstcount_o    => s_burstcount,
+      m_readdatavalid_i => s_readdatavalid,
+      m_readdata_i      => s_readdata
+    );
+
+
+  --------------------------------
+  -- Instantiate Avalon Slave
+  --------------------------------
+
+  avm_slave_sim_inst : entity work.avm_slave_sim
+    generic map (
+      G_BURST_WIDTH => G_BURST_WIDTH,
+      G_NAME        => "SLAVE",
+      G_DEBUG       => G_DEBUG,
+      G_ADDR_SIZE   => G_MASTER_ADDR_SIZE,
+      G_DATA_SIZE   => G_MASTER_DATA_SIZE
+    )
+    port map (
+      clk_i             => clk,
+      rst_i             => rst,
+      s_waitrequest_o   => m_waitrequest,
+      s_write_i         => m_write,
+      s_read_i          => m_read,
+      s_address_i       => m_address,
+      s_writedata_i     => m_writedata,
+      s_byteenable_i    => m_byteenable,
+      s_burstcount_i    => m_burstcount,
+      s_readdatavalid_o => m_readdatavalid,
+      s_readdata_o      => m_readdata
+    );
+
+end architecture simulation;
+
---
+++ src/avm/avm_decrease.vhd
@@ -0,0 +1,151 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+-- This reduces the data width of an Avalon Memory Map interface.
+
+entity avm_decrease is
+  generic (
+    G_BURST_WIDTH         : positive := 8;
+    G_SLAVE_ADDRESS_SIZE  : positive;
+    G_SLAVE_DATA_SIZE     : positive; -- Must be a multiple of G_MASTER_DATA_SIZE
+    G_MASTER_ADDRESS_SIZE : positive;
+    G_MASTER_DATA_SIZE    : positive
+  );
+  port (
+    clk_i             : in    std_logic;
+    rst_i             : in    std_logic;
+
+    -- Slave interface (input)
+    s_waitrequest_o   : out   std_logic;
+    s_write_i         : in    std_logic;
+    s_read_i          : in    std_logic;
+    s_address_i       : in    std_logic_vector(G_SLAVE_ADDRESS_SIZE - 1 downto 0);
+    s_writedata_i     : in    std_logic_vector(G_SLAVE_DATA_SIZE - 1 downto 0);
+    s_byteenable_i    : in    std_logic_vector(G_SLAVE_DATA_SIZE / 8 - 1 downto 0);
+    s_burstcount_i    : in    std_logic_vector(G_BURST_WIDTH - 1 downto 0);
+    s_readdata_o      : out   std_logic_vector(G_SLAVE_DATA_SIZE - 1 downto 0);
+    s_readdatavalid_o : out   std_logic;
+
+    -- Master interface (output)
+    m_waitrequest_i   : in    std_logic;
+    m_write_o         : out   std_logic;
+    m_read_o          : out   std_logic;
+    m_address_o       : out   std_logic_vector(G_MASTER_ADDRESS_SIZE - 1 downto 0);
+    m_writedata_o     : out   std_logic_vector(G_MASTER_DATA_SIZE - 1 downto 0);
+    m_byteenable_o    : out   std_logic_vector(G_MASTER_DATA_SIZE / 8 - 1 downto 0);
+    m_burstcount_o    : out   std_logic_vector(G_BURST_WIDTH - 1 downto 0);
+    m_readdata_i      : in    std_logic_vector(G_MASTER_DATA_SIZE - 1 downto 0);
+    m_readdatavalid_i : in    std_logic
+  );
+end entity avm_decrease;
+
+architecture synthesis of avm_decrease is
+
+  constant C_RATIO        : integer                                               := G_SLAVE_DATA_SIZE / G_MASTER_DATA_SIZE;
+  constant C_ZERO_DATA    : std_logic_vector(G_MASTER_DATA_SIZE - 1 downto 0)     := (others => '0');
+  constant C_ZERO_BYTE_EN : std_logic_vector(G_MASTER_DATA_SIZE / 8 - 1 downto 0) := (others => '0');
+
+  signal   s_write      : std_logic;
+  signal   s_read       : std_logic;
+  signal   s_address    : std_logic_vector(G_SLAVE_ADDRESS_SIZE - 1 downto 0);
+  signal   s_writedata  : std_logic_vector(G_SLAVE_DATA_SIZE - 1 downto 0);
+  signal   s_byteenable : std_logic_vector(G_SLAVE_DATA_SIZE / 8 - 1 downto 0);
+  signal   s_burstcount : std_logic_vector(G_BURST_WIDTH - 1 downto 0);
+
+  type     state_type is (IDLE_ST, WRITING_ST, READING_ST);
+  signal   state : state_type                                                     := IDLE_ST;
+
+  signal   s_write_pos : integer range 0 to C_RATIO - 1                           := 0;
+  signal   s_read_pos  : integer range 0 to C_RATIO - 1                           := 0;
+
+begin
+
+  assert C_RATIO > 1
+    severity failure;
+  assert G_SLAVE_DATA_SIZE = C_RATIO * G_MASTER_DATA_SIZE
+    severity failure;
+  assert G_SLAVE_DATA_SIZE * (2 ** G_SLAVE_ADDRESS_SIZE) = G_MASTER_DATA_SIZE * (2 ** G_MASTER_ADDRESS_SIZE)
+    severity failure;
+
+  fsm_proc : process (clk_i)
+  begin
+    if rising_edge(clk_i) then
+      s_readdatavalid_o <= '0';
+
+      if m_waitrequest_i = '0' then
+        s_write <= '0';
+        s_read  <= '0';
+      end if;
+
+      if m_readdatavalid_i = '1' then
+        s_readdata_o(G_MASTER_DATA_SIZE * s_read_pos + G_MASTER_DATA_SIZE - 1 downto G_MASTER_DATA_SIZE * s_read_pos) <= m_readdata_i;
+
+        if s_read_pos + 1 = C_RATIO then
+          s_read_pos        <= 0;
+          s_readdatavalid_o <= '1';
+        else
+          s_read_pos <= s_read_pos + 1;
+        end if;
+      end if;
+
+      case state is
+
+        when IDLE_ST =>
+          if (s_write_i = '1' or s_read_i = '1') and s_waitrequest_o = '0' then
+            s_write      <= s_write_i;
+            s_read       <= s_read_i;
+            s_address    <= s_address_i;
+            s_writedata  <= s_writedata_i;
+            s_byteenable <= s_byteenable_i;
+            s_burstcount <= std_logic_vector(to_unsigned(C_RATIO * to_integer(unsigned(s_burstcount_i)), G_BURST_WIDTH));
+            if s_write_i = '1' then
+              s_write_pos <= 0;
+              state       <= WRITING_ST;
+            elsif s_read_pos /= 0 or m_readdatavalid_i = '1' then
+              state <= READING_ST;
+            else
+              s_read_pos <= 0;
+            end if;
+          end if;
+
+        when WRITING_ST =>
+          if m_waitrequest_i = '0' then
+            s_write_pos <= s_write_pos + 1;
+
+            -- Preserve value from previous clock cycle
+            s_write     <= s_write;
+
+            if s_write_pos + 2 = C_RATIO then
+              state <= IDLE_ST;
+            end if;
+          end if;
+
+        when READING_ST =>
+          if s_read_pos = 0 then
+            state <= IDLE_ST;
+          end if;
+
+      end case;
+
+      if rst_i = '1' then
+        s_write    <= '0';
+        s_read     <= '0';
+        s_read_pos <= 0;
+        state      <= IDLE_ST;
+      end if;
+    end if;
+  end process fsm_proc;
+
+  m_write_o                                                                                  <= s_write;
+  m_read_o                                                                                   <= s_read;
+  m_address_o(G_MASTER_ADDRESS_SIZE - 1 downto G_MASTER_ADDRESS_SIZE - G_SLAVE_ADDRESS_SIZE) <= s_address;
+  m_address_o(G_MASTER_ADDRESS_SIZE - G_SLAVE_ADDRESS_SIZE - 1 downto 0)                     <= (others => '0');
+  m_writedata_o                                                                              <= s_writedata(G_MASTER_DATA_SIZE * s_write_pos + G_MASTER_DATA_SIZE - 1 downto G_MASTER_DATA_SIZE * s_write_pos);
+  m_byteenable_o                                                                             <= s_byteenable(G_MASTER_DATA_SIZE / 8 * s_write_pos + G_MASTER_DATA_SIZE / 8 - 1 downto G_MASTER_DATA_SIZE / 8 * s_write_pos);
+  m_burstcount_o                                                                             <= s_burstcount;
+  s_waitrequest_o                                                                            <= ((m_write_o or m_read_o) and m_waitrequest_i) when state = IDLE_ST else
+                                                                                                '1';
+
+end architecture synthesis;
+
